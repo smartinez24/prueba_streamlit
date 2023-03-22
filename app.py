480dedadb4b6734420db9274589463f6095ac317
smartinez24 <martinezacademico88@gmail.com>
2023-03-22 13:10:33 -0500

    Prueba

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st 
+
+st.image('https://i.pinimg.com/564x/dc/76/44/dc7644221aea945df996214141e853b7.jpg')
+
+st.title('Hola mundo')
+
+st.markdown('***')
+
+st.markdown('Practica streamlit')
